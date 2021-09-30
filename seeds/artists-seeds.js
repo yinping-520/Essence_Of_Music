10f2f73eca00e76b09eb3e699c7b9587e2594913
Alexis Guzman <alexisguzman157@outlook.com>
2021-09-30 10:03:12 -0400

    updated seeds

--- seeds/artists-seeds.js
+++ seeds/artists-seeds.js
@@ -2,11 +2,139 @@ const { Artists } = require('../models');
 
 const artistData = [
     {
+        artist: 'Lil Nas X',
+        genre_id: 1,
+    },
+    {
+        artist: 'Doja Cat',
+        genre_id: 1,
+    },
+    {
+        artist: 'Ed Sheeran',
+        genre_id: 1,
+    },
+    {
+        artist: 'Coldplay',
+        genre_id: 1,
+    },
+    {
+        artist: 'Chloe',
+        genre_id: 1,
+    },
+    {
+        artist: 'Billie Ellish',
+        genre_id: 1,
+    },
+    {
         artist: 'The Weeknd',
         genre_id: 1,
     },
     {
+        artist: 'Farruko',
+        genre_id: 1,
+    },
+    {
+        artist: 'Dua Lipa',
+        genre_id: 1,
+    },
+    {
+        artist: 'Glass Animals',
+        genre_id: 1,
+    },
+    {
+        artist: 'Luke Combs',
+        genre_id: 2,
+    },
+    {
+        artist: 'Morgan Evans',
+        genre_id: 2,
+    },
+    {
+        artist: 'Midland',
+        genre_id: 2,
+    },
+    {
+        artist: 'Billy Ray Cyrus',
+        genre_id: 2,
+    },
+    {
+        artist: 'Chris Stapleton',
+        genre_id: 2,
+    },
+    {
+        artist: 'Kenny Chesney',
+        genre_id: 2,
+    },
+    {
+        artist: 'Cody Jenkins',
+        genre_id: 2,
+    },
+    {
+        artist: 'Chris Young',
+        genre_id: 2,
+    },
+    {
+        artist: 'Mitchel Tempanny',
+        genre_id: 2,
+    },
+    {
+        artist: 'Jmmie Allen',
+        genre_id: 2,
+    },
+    {
+        artist: 'Drake',
+        genre_id: 3,
+    },
+    {
+        artist: 'Meek Mill',
+        genre_id: 3,
+    },
+    {
+        artist: 'Dababy',
+        genre_id: 3,
+    },
+    {
+        artist: 'Gunna',
+        genre_id: 3,
+    },
+    {
+        artist: 'J.Cole',
+        genre_id: 3,
+    },
+    {
+        artist: 'Sleepy Hallow',
+        genre_id: 3,
+    },
+    {
+        artist: 'Lil tecca',
+        genre_id: 3,
+    },
+    {
+        artist: 'Wokesha',
+        genre_id: 3,
+    },
+    {
+        artist: 'Kanye West',
+        genre_id: 3,
+    },
+    {
+        artist: 'Joyner Lucas',
+        genre_id: 3,
+    },
+    {
+        artist: 'Café Tacuba',
+        genre_id: 4,
+    },
+    {
+        artist: 'La Sonora Dinamita',
+        genre_id: 4,
+    },
+    {
         artist: 'Miguel Bosé',
+        genre_id: 4,
+    },
+    {
+        artist: 'Daddy Yankee',
         genre_id: 4,
     },
 ];
--- seeds/song-seeds.js
+++ seeds/song-seeds.js
@@ -3,22 +3,220 @@ const { Song } = require('../models');
 
 const songData = [
     {
+        song:'',
+        lyrics:'',
+        youtube:'',
+        album_id: 1
+    },
+    {
+        song:'',
+        lyrics:'',
+        youtube:'',
+        album_id: 2
+    },
+    {
+        song:'',
+        lyrics:'',
+        youtube:'',
+        album_id: 3
+    },
+    {
+        song:'',
+        lyrics:'',
+        youtube:'',
+        album_id: 4
+    },
+    {
+        song:'',
+        lyrics:'',
+        youtube:'',
+        album_id: 5
+    },
+    {
+        song:'',
+        lyrics:'',
+        youtube:'',
+        album_id: 6
+    },
+    {
         song:'Take My Breath Away',
         lyrics:'I saw the fire in your eyes I saw the fire when I look into your eyes You tell me things you wanna try',
         youtube:'https://www.youtube.com/watch?v=rhTl_OyehF8',
-        album_id: 1
+        album_id: 7
+    },
+    {
+        song:'',
+        lyrics:'',
+        youtube:'',
+        album_id: 8
+    },
+    {
+        song:'',
+        lyrics:'',
+        youtube:'',
+        album_id: 9
+    },
+    {
+        song:'',
+        lyrics:'',
+        youtube:'',
+        album_id: 10
+    },
+    {
+        song:'',
+        lyrics:'',
+        youtube:'',
+        album_id: 11
+    },
+    {
+        song:'',
+        lyrics:'',
+        youtube:'',
+        album_id: 12
+    },
+    {
+        song:'',
+        lyrics:'',
+        youtube:'',
+        album_id: 13
+    },
+    {
+        song:'',
+        lyrics:'',
+        youtube:'',
+        album_id: 14
+    },
+    {
+        song:'',
+        lyrics:'',
+        youtube:'',
+        album_id: 15
+    },
+    {
+        song:'',
+        lyrics:'',
+        youtube:'',
+        album_id: 16
+    },
+    {
+        song:'',
+        lyrics:'',
+        youtube:'',
+        album_id: 17
+    },
+    {
+        song:'',
+        lyrics:'',
+        youtube:'',
+        album_id: 18
+    },
+    {
+        song:'',
+        lyrics:'',
+        youtube:'',
+        album_id: 19
+    },
+    {
+        song:'',
+        lyrics:'',
+        youtube:'',
+        album_id: 20
+    },
+    {
+        song:'',
+        lyrics:'',
+        youtube:'',
+        album_id: 21
+    },
+    {
+        song:'',
+        lyrics:'',
+        youtube:'',
+        album_id: 22
+    },
+    {
+        song:'',
+        lyrics:'',
+        youtube:'',
+        album_id: 23
+    },
+    {
+        song:'',
+        lyrics:'',
+        youtube:'',
+        album_id: 24
+    },
+    {
+        song:'',
+        lyrics:'',
+        youtube:'',
+        album_id: 25
+    },
+    {
+        song:'',
+        lyrics:'',
+        youtube:'',
+        album_id: 26
+    },
+    {
+        song:'',
+        lyrics:'',
+        youtube:'',
+        album_id: 27
+    },
+    {
+        song:'',
+        lyrics:'',
+        youtube:'',
+        album_id: 28
+    },
+    {
+        song:'',
+        lyrics:'',
+        youtube:'',
+        album_id: 29
+    },
+    {
+        song:'',
+        lyrics:'',
+        youtube:'',
+        album_id: 30
+    },
+    {
+        song:'',
+        lyrics:'',
+        youtube:'',
+        album_id: 31
+    },
+    {
+        song:'',
+        lyrics:'',
+        youtube:'',
+        album_id: 31
+    },
+    {
+        song:'',
+        lyrics:'',
+        youtube:'',
+        album_id: 32
     },
     {
         song:'Amanete Bandido',
         lyrics:'Yo seré el viento que va Navegaré por tu oscuridadTu rocío, beso frío, que me quemará',
         youtube:'https://www.youtube.com/watch?v=lA9p_fpUxnY',
-        album_id: 2
+        album_id: 33
     },
     {
         song:'Thing',
         lyrics:'Yo seré el viento que va Navegaré por tu oscuridadTu rocío, beso frío, que me quemará',
         youtube:'https://www.youtube.com/watch?v=lA9p_fpUxnY',
-        album_id: 2
+        album_id: 33
+    },
+    {
+        song:'',
+        lyrics:'',
+        youtube:'',
+        album_id: 34
     },
 ];
 
