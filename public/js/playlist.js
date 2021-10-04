fc4cd8c23a32519a99ce11ecd14fc863509c47cd
yinping-520 <85038945+yinping-520@users.noreply.github.com>
2021-10-04 15:24:15 -0400

    save and delete worked in both playlist and artist

--- public/js/playlist.js
+++ public/js/playlist.js
@@ -1,19 +1,29 @@
 const albumEl = document.querySelector(".album");
 albumEl.addEventListener("click", function (event) {
-  event.preventDefault()
+  event.preventDefault();
   const element = event.target;
   if (element.textContent === "♡") {
-    element.textContent = "❤"
-    const artist_id = element.getAttribute('data-id');
-    const youtube = element.getAttribute('data-content');
-    const song_name = element.getAttribute('data-name');
-    const audio = element.getAttribute('data-audio');
-    fetch('/api/favorites', {
-      method: 'POST',
+    element.textContent = "❤";
+    const artist_id = element.getAttribute("data-id");
+    const youtube = element.getAttribute("data-content");
+    const song_name = element.getAttribute("data-name");
+    const audio = element.getAttribute("data-audio");
+    fetch("/api/favorites", {
+      method: "POST",
       body: JSON.stringify({ artist_id, youtube, song_name, audio }),
-      headers: { 'Content-Type': 'application/json' },
-  }).then(response=>{response.json()})
-  .then(data => {console.log(data)})
+      headers: { "Content-Type": "application/json" },
+    })
+      .then(response => response)
+  } else if (element.textContent === "❤") {
+    element.textContent = "♡";
+    const artist_id = element.getAttribute("data-id");
+    fetch(`/api/favorites/${artist_id}`, {
+      method: "DELETE",
+    }).then((response) => {
+      console.log(response);
+      if (response.ok) {
+        document.location.replace(`/artist/${artist_id}`);
+      }
+    });
   }
 });
-
